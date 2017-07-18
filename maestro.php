e156c3ba35187a5ceeeb675ff51fb4f83b8899fe
ivan carapia <icarapia94@gmail.com>
2017-07-18 10:03:35 -0500

    maestro

--- maestro.php
+++ maestro.php
@@ -4,13 +4,11 @@
 <meta charset="utf-8">
 	<title>Registro Maestro</title>
 <body>
-<h2>Bienvenido, en esta sección te podrás registrar para asignar calificaciones.</h2>
-<h2>Nombre <input type="text" name="nombreAlumno"></h2>
-<h2>Periodo <input type="text" name="periodo"></h2>
-<h2>Correo <input type="text" name="correo"></h2>
-<h2>Telefono <input type="text" name="telefono"></h2>
-<h2>Télefono de Emergencia <input type="text" name="telEmergencia"></h2>
-<h2>Dirección <input type="text" name="direccion"></h2>
+<h2>Registro de Maestros</h2>
+		<h2>Nombre <input type="text" name="nombreAlumno"  required></h2>
+		<h2>Correo  <input type="text" name="periodo" required></h2>
+		<h2>Contraseña<input type="text" name="correo" required></h2>
+		<input type="button" value="Registrar">
 <input type="button" value="Registrar">
 </body>
 </html>
