bcd857ab093ea6e8f8e765d4cdebc5296db7e678
ivan carapia <icarapia94@gmail.com>
2017-09-21 20:00:30 -0500

    modificaciones

--- interfazalumno.php
+++ interfazalumno.php
@@ -1,78 +1,50 @@
 <?php 
 session_start();
-
- #mandamos la conexion a la base de datos 
     require_once('conexion.php');
- 
     #si el usuario cerro sesion
     if (isset($_GET['salir'])) {
         #eliminar variables de sesion
        session_unset();
-
     }
-    
-
-    
-
   if (isset($_SESSION['expedientea'])) {  
     $nip=$_SESSION['expedientea'];
   $userData = "SELECT nombreAlumno FROM alumno
                          WHERE expediente= {$nip}";
-
  $usuario = $conexion->query($userData)->fetch_object();
-  
- ?>
-
+   ?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Sesión Usuario</title>
+  <title>Sesión de <?php echo $usuario->nombreAlumno; ?></title>
 </head>
 <body>
-<?php echo "bienvenido ".$usuario->nombreAlumno ;  ?>
+  Datos Personales
+    </br> 
+  <?php echo "bienvenido ".$usuario->nombreAlumno ;  ?>
 
-
-<?php 
-
-#hacemos una consulta que nos muestre las calificaciones
-$vercalificacion = "SELECT * FROM calificaciones, materia
-where idAlumno= (SELECT expediente from alumno where expediente= {$nip});";
-#mandamos la consulta a la bd de datos 
-$resultadopaq = mysqli_query($conexion, $vercalificacion)
-
-// $vermat = "SELECT * FROM materia
-//where idMateria= (SELECT idMateria from calificaciones where idMateria= 1);";
-//$resultadomat = mysqli_query($conexion, $vermat)
-
-
-
+  <?php 
+  #hacemos una consulta que nos muestre las calificaciones
+  $vercalificacion = "SELECT * FROM calificaciones, materia
+  where idAlumno= (SELECT expediente from alumno where expediente= {$nip});";
+  #mandamos la consulta a la bd de datos 
+  $resultadopaq = mysqli_query($conexion, $vercalificacion)
  ?>
  <center>
- <table>
-  <tr>
-  <th>Materia</th>
-  <th>Calificacion</th>
-  
-  </tr>
-
-   <tr>
-  
-        <?php 
-  while ($paq= $resultadopaq->fetch_object()) { ?>
-
-  <?php  $vermat = "SELECT * FROM materia ;";
-  $resultadomat = mysqli_query($conexion, $vermat)
-  //$paq= $resultadomat->fetch_object()
-   ?>
-    <td><?php echo $paq->nombreMateria;  ?></td>
-  <td><?php echo $paq->calFinal;;
+  <table>
+    <tr>
+      <th>Materia</th>
+      <th>Calificacion</th>
+    </tr>
+    <tr>
+      <?php 
+      while ($paq= $resultadopaq->fetch_object()) { ?>    
+        <td><?php echo $paq->nombreMateria;  ?></td>
+        <td><?php echo $paq->calFinal;
       ?></td>
-   </tr>
-
-   <?php }  ?>
- </table>
-  </center>
-
+    </tr>
+    <?php }  ?>
+  </table>
+ </center>  
 </body>
 </html>
 <?php  }else {header("location:loginAlumno.php"); } ?>
